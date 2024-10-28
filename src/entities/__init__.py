8ac7c9f6c45c74cd5fb1a62106a5fa9cd6c066af
AbdullaevG <abd.gm90@yandex.ru>
2024-10-28 23:11:19 +0300

    modify init for entities

--- src/entities/__init__.py
+++ src/entities/__init__.py
@@ -1,6 +1,8 @@
 from .split_params import SplittingParams
+from .training_pipeline_params import (
+    TrainingPipelineParams,
+    read_training_pipeline_params,
+)
+from .training_params import TrainingParams
 
-
-__all__ = [
-    'SplittingParams'
-]
+__all__ = ["SplittingParams", "TrainingPipelineParams", "read_training_pipeline_params", "TrainingParams"]
