c8a0843f7064b374a9488c116db2418f8c06ffa0
AbdullaevG <abd.gm90@yandex.ru>
2024-10-28 23:12:09 +0300

    add models code

---
+++ src/models/__init__.py
@@ -0,0 +1,6 @@
+from .train_model import train_model, model_predict
+
+__all__ =[
+    'train_model',
+    'model_predict'
+]
---
+++ src/models/train_model.py
@@ -0,0 +1,16 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from src.entities import TrainingParams
+
+
+def train_model(features: np.array, target: np.array, training_params: TrainingParams):
+    if training_params.model_type == "LogisticRegression":
+        model = LogisticRegression(random_state=training_params.random_state)
+    else:
+        raise NotImplementedError()
+    model.fit(features, target)
+    return model
+    
+def model_predict(features: np.array, model: LogisticRegression)-> np.array:
+    predict = model.predict(features)
+    return predict
