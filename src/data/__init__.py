6bb0ff40986f05adf84fdc2e1ac7db80c9f84e33
AbdullaevG <abd.gm90@yandex.ru>
2025-02-23 19:58:03 +0300

    fix bug: change data/__init__

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,2 +1,4 @@
-from .data_utils import read_data, split_train_validate_data, load_data_from_s3
+from .make_datasets import read_data, split_train_validate_data, load_data_from_s3
+
+
 all = ["read_data", "split_train_validate_data", "load_data_from_s3"]
